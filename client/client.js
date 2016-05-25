467e5c2de622a3a08db5beba39bfbfc0cb314d45
John Heher <jheher@gmail.com>
2016-05-24 21:01:57 -0500

    Initial build of overall system

---
+++ client/client.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { render } from "react-dom"
+import { Router, Route, IndexRoute, browserHistory } from "react-router"
+import { Provider } from 'react-redux'
+import store, { history } from './store'
+
+import App from "./components/App"
+import Watch from "./components/Watch"
+
+import css from './css/index.sass'
+
+const router = (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Watch}></IndexRoute>
+      </Route>
+    </Router>
+  </Provider>
+)
+
+const app = document.getElementById('app')
+
+render(router, app)
---
+++ client/components/Watch.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+export default class Watch extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+
+    return (
+      <div className="watch">
+        <h1>Watch</h1>
+      </div>
+    );
+  }
+}
